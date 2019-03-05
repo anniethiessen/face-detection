0873b44bc486f536f1562a828ed0cbb5d36e2a5f
Annie Thiessen <anniethiessen79@gmail.com>
2019-03-04 17:00:51 -0700

    Initial commit:  Add main module for Viola-Jones haar cascade

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+original_image = cv.imread('~/Desktop/img4593_54.jpg')
+grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+face_cascade = cv.CascadeClassifier(
+    '//Applications/anaconda3/envs/face-detection-env/bin/'
+    'haarcascade_frontalface_alt.xml'
+)
+detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+for (column, row, width, height) in detected_faces:
+    cv.rectangle(
+        original_image,
+        (column, row),
+        (column + width, row + height),
+        (0, 255, 0),
+        2
+    )
+
+cv.imshow('Image', original_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
